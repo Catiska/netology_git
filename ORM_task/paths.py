f5def4bdff7cc226966b9a6567e6888aa41c04c0
Catiska <katrinka5@yandex.ru>
2022-11-12 10:16:10 +0300

    Fix paths

--- ORM_task/paths.py
+++ ORM_task/paths.py
@@ -1,4 +1,4 @@
-DSN = 'postgresql://postgres:elis@localhost:5432/bookstore'
+DSN = 'postgresql://postgres:postgres@localhost:5432/bookstore'
 DB_CLIENTS = "clients_db"
 USER_CLIENTS = "postgres"
 PASSWORD_CLIENTS = "postgres"
